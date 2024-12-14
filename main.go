eba74254236d5a4f4f21c02b828e00d5ed05bdbb
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2024-12-14 19:01:23 +0700

    initial commit & connection to database

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/vanstartup?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Connection database successfully")
+}
