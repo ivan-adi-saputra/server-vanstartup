ad0f1d96cbb6bec1ea6556f484f29ec0fcc6516f
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2025-01-07 21:35:01 +0700

    implementation api update campaigns

--- campaign/service.go
+++ campaign/service.go
@@ -1,9 +1,12 @@
 package campaign
 
+import "errors"
+
 type Service interface {
 	FindCampaigns(userID int) ([]Campaign, error)
 	FindCampaign(input GetCampaignDetailByID) (Campaign, error)
 	SaveCampaign(input CreateCampaignInput) (Campaign, error)
+	UpdateCampaign(inputID GetCampaignDetailByID, inputData CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -57,4 +60,28 @@ func (s *service) SaveCampaign(input CreateCampaignInput) (Campaign, error) {
 	}
 
 	return newCampaign, nil
+}
+
+func (s *service) UpdateCampaign(inputID GetCampaignDetailByID, inputData CreateCampaignInput) (Campaign, error) {
+	campaign, err := s.r.FindByID(inputID.ID)
+	if err != nil {
+		return campaign, err
+	}
+
+	if campaign.UserID != inputData.User.ID {
+		return campaign, errors.New("not an owner of the campaign")
+	}
+
+	campaign.Name = inputData.Name
+	campaign.ShortDescription = inputData.ShortDescription
+	campaign.Description = inputData.Description
+	campaign.Perks = inputData.Perks
+	campaign.GoalAmount = inputData.GoalAmount
+
+	updatedCampaign, err := s.r.Update(campaign)
+	if err != nil {
+		return updatedCampaign, err
+	}
+
+	return updatedCampaign, nil
 }
